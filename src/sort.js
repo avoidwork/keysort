a25bfb670becce5035da91af938ac79dbbbed348
Mark Sost <mark.sost@vice.com>
2013-08-28 09:58:37 -0400

    Added in  argument to allow sorting values to be cast to integers before being sorted.  Created test.html to provide a test case to validate output against

--- src/sort.js
+++ src/sort.js
@@ -5,12 +5,16 @@
  * @param  {Array}  obj   Array to sort
  * @param  {String} query Sort query, e.g. "name, age desc, country"
  * @param  {String} sub   [Optional] Key which holds data, e.g. "{data: {}}" = "data"
+ * @param  {Boolean} cast   [Optional] If true, will cast the values to integers before comparing them
+                                                                   Useful if you need to compare numbers represented as strings
  * @return {Array}        Sorted Array
  */
-function sort ( obj, query, sub ) {
+function sort ( obj, query, sub, cast ) {
 	query       = query.replace( /\s*asc/g, "" ).replace( /\s*desc/ig, " desc" );
 	var queries = explode( query ).map( function ( i ) { return i.split( " " ); }),
 	    sorts   = [];
+
+	    cast = cast !== undefined;
 
 	if ( sub && sub !== "" ) {
 		sub = "." + sub;
@@ -20,15 +24,22 @@ function sort ( obj, query, sub ) {
 	}
 
 	each( queries, function ( i ) {
-		var desc = i[1] === "desc";
+		var desc = i[1] === "desc",
+		a = "a" + sub + "[\"" + i[0] + "\"]",
+		b = "b" + sub + "[\"" + i[0] + "\"]";
+
+		if(cast){
+			sorts.push(a + " = " + a + ".toString().indexOf(\".\") === -1 ? parseInt(" + a + ", 10) : parseFloat(" + a + ");");
+			sorts.push(b + " = " + b + ".toString().indexOf(\".\") === -1 ? parseInt(" + b + ", 10) : parseFloat(" + b + ");");
+		}
 
 		if ( !desc ) {
-			sorts.push( "if ( a" + sub + "[\"" + i[0] + "\"] < b" + sub + "[\"" + i[0] + "\"] ) return -1;" );
-			sorts.push( "if ( a" + sub + "[\"" + i[0] + "\"] > b" + sub + "[\"" + i[0] + "\"] ) return 1;" );
+			sorts.push( "if ( " + a + " < " + b + " ) return -1;" );
+			sorts.push( "if ( " + a + " > " + b + " ) return 1;" );
 		}
 		else {
-			sorts.push( "if ( a" + sub + "[\"" + i[0] + "\"] < b" + sub + "[\"" + i[0] + "\"] ) return 1;" );
-			sorts.push( "if ( a" + sub + "[\"" + i[0] + "\"] > b" + sub + "[\"" + i[0] + "\"] ) return -1;" );
+			sorts.push( "if ( " + a + " < " + b + " ) return 1;" );
+			sorts.push( "if ( " + a + " > " + b + " ) return -1;" );
 		}
 	});
 
